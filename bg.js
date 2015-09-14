5a05845a4fdcb9bf340505d940ec8e7209292f1e
VixinG <v1x1n6@gmail.com>
2015-09-14 03:08:22 +0200

    Added servers and modes
    Added Level3 CDN and Local (no redirect) servers to the list.
    Added Simple Mode (right click toggle)

--- bg.js
+++ bg.js
@@ -1,86 +1,126 @@
+var simple = false;
+
+var using = 2;
 var search = /vid.*hls.*(\/hls.*.ts)/;
 
 var countries = {
-    "US": "United States",
-    "SE": "Sweden",
-    "UK": "United Kingdom",
-    "NL": "Netherlands",
-    "FR": "France",
-    "DE": "Germany",
-    "CZ": "Czech Republic",
+	"none": "Default/None",
+	"GL": "Global Server",
+	"US": "United States",
+	"SE": "Sweden",
+	"UK": "United Kingdom",
+	"NL": "Netherlands",
+	"FR": "France",
+	"DE": "Germany",
+	"CZ": "Czech Republic",
 };
 
 var replacements = [
-        ["US",    "San Francisco",    "video-edge-2ca3e4.sfo01.hls.ttvnw.net"],
-        ["US",    "Seattle",          "video-edge-7ea618.sea01.hls.ttvnw.net"],
-        ["US",    "San Jose",         "video-edge-7e96ac.sjc01.hls.ttvnw.net"],
-        ["US",    "Chicago",          "video-edge-835140.ord02.hls.ttvnw.net"],
-        ["US",    "Washington",       "video20.iad02.hls.ttvnw.net"],
-        ["US",    "New York",         "video20.jfk01.hls.ttvnw.net"],
-        ["US",    "Los Angeles",      "video20.lax01.hls.ttvnw.net"],
-        ["US",    "Dallas",           "video20.dfw01.hls.ttvnw.net"],
-        
-        ["SE",    "Stockholm",        "video20.arn01.hls.ttvnw.net"],
-        
-        ["UK",    "London",           "video20.lhr02.hls.ttvnw.net"],
-        
-        ["NL",    "Amsterdam",        "video20.ams01.hls.ttvnw.net"],
-        
-        ["FR",    "Paris",            "video16.cdg01.hls.ttvnw.net"],
-        
-        ["DE",    "Frankfurt",        "video20.fra01.hls.ttvnw.net"],
-        
-        ["CZ",    "Prague",           "video20.prg01.hls.ttvnw.net"],
-    ];
-var using = 0;
+	["none", "Local server", "none"],
+	["GL", "Level3 cdn", "twitch2-live.hls.adaptive.level3.net"],
+
+	["US-1", "San Francisco", "video-edge-2ca3e4.sfo01.hls.ttvnw.net"],
+	["US-2", "Seattle", "video-edge-7ea618.sea01.hls.ttvnw.net"],
+	["US-3", "San Jose", "video-edge-7e96ac.sjc01.hls.ttvnw.net"],
+	["US-4", "Chicago", "video-edge-835140.ord02.hls.ttvnw.net"],
+	["US-5", "Washington", "video20.iad02.hls.ttvnw.net"],
+	["US-6", "New York", "video20.jfk01.hls.ttvnw.net"],
+	["US-7", "Los Angeles", "video20.lax01.hls.ttvnw.net"],
+	["US-8", "Dallas", "video20.dfw01.hls.ttvnw.net"],
+
+	["SE", "Stockholm", "video20.arn01.hls.ttvnw.net"],
+
+	["UK", "London", "video20.lhr02.hls.ttvnw.net"],
+
+	["NL", "Amsterdam", "video20.ams01.hls.ttvnw.net"],
+
+	["FR", "Paris", "video16.cdg01.hls.ttvnw.net"],
+
+	["DE", "Frankfurt", "video20.fra01.hls.ttvnw.net"],
+
+	["CZ", "Prague", "video20.prg01.hls.ttvnw.net"]
+];
 
 var per_country = {};
 for (var i = 0; i < replacements.length; i++) {
-    var country = replacements[i][0];
-    if (typeof per_country[country] == 'undefined') {
-        per_country[country] = [];
-    }
-    per_country[country].push(i);
+	var country = replacements[i][0].replace(/^([a-zA-Z]+).*$/,"$1");
+	if (typeof per_country[country] == 'undefined') {
+		per_country[country] = [];
+	}
+	per_country[country].push(i);
 }
 
 function updateButton() {
-    var cluster = replacements[using][2].replace(/([\s\S]*([a-z]{3})0[\s\S]*)/, "$2");
-    chrome.browserAction.setBadgeText({
-        text: cluster.toString()
-    });
+	var cluster = replacements[using][0];
+	chrome.browserAction.setBadgeText({
+		text: cluster.toString()
+	});
 }
 
 function changeServer(id) {
-    using = id;
-    updateButton();
+	using = parseInt(id);
+	updateButton();
+}
+
+function click() {
+	if (using > replacements.length -2)
+		using = 0
+	else
+		using += 1;
+	updateButton();
 }
 
 function init() {
-    chrome.webRequest.onBeforeRequest.addListener(
-        function(info) {
-            if (info.url.indexOf(".ts") > -1){
-                var conf;
-                var subject = info.url;
-                
-                conf = replacements[using][2];
-                subject = subject.replace(search, conf + "$1");
-                
-                if (subject !== info.url) {
-                    return {
-                        redirectUrl: subject
-                    };
-                }
-                }
-            else
-                return {};
-        }, {
-            urls: [
-                "*://*/*"
-            ]
-        }, ["blocking"]
-    );
+	chrome.webRequest.onBeforeRequest.addListener(
+		function(info) {
+			if (info.url.indexOf(".ts") > -1) {
+				var conf;
+				var subject = info.url;
 
-    updateButton();
+				if (using == 0) {
+					return {};
+				}
+
+				conf = replacements[using][2];
+				subject = subject.replace(search, conf + "$1");
+
+				if (subject !== info.url) {
+					return {
+						redirectUrl: subject
+					};
+				}
+			} else
+				return {};
+		}, {
+			urls: [
+				"*://*/*"
+			]
+		}, ["blocking"]
+	);
+
+	updateButton();
 }
+
+function resimple() {
+	if(simple) {
+		chrome.contextMenus.update("toggle", {"title":"Simple Mode"});
+		chrome.browserAction.setPopup({popup: "popup.html"});
+		simple = false;
+	}
+	else {
+		chrome.contextMenus.update("toggle", {"title":"List Mode"});
+		chrome.browserAction.setPopup({popup: ""});
+		simple = true;
+	}
+}
+
+chrome.contextMenus.create({
+	"id": "toggle",
+	"title": "Simple Mode",
+	"contexts": ["all"],
+	"onclick": resimple
+});
+
+chrome.browserAction.onClicked.addListener(click);
 
 init();
--- popup.js
+++ popup.js
@@ -1,28 +1,27 @@
 var bg = chrome.extension.getBackgroundPage();
 
 document.addEventListener('DOMContentLoaded', function() {
-    document.body.onclick = function(e) {
-        e = e.target;
-        if (e.className && e.className.indexOf('server') != -1) {
-            var id = e.getAttribute("data-id");
-            bg.changeServer(id);
-            
-            document.getElementsByClassName('server--selected')[0].classList.remove('server--selected');
-            e.classList.add('server--selected');
-        }
-    }
+	document.body.onclick = function(e) {
+		e = e.target;
+		if (e.className && e.className.indexOf('server') != -1) {
+			var id = e.getAttribute("data-id");
+			bg.changeServer(id);
 
-    var html = '';
-    for (var key in bg.per_country) {
-        var servers = bg.per_country[key];
-        for (var i = 0; i < servers.length; i++) {
-            if (i == 0) {
-                html += '<div class="country">' + bg.countries[key] + '</div>';
-            }
-            
-            html += '<div class="server ' + (bg.using == servers[i] ? 'server--selected':'') + '" data-id="' + servers[i] + '">'
-                + bg.replacements[servers[i]][1] + '</div>';
-        }
-    };
-    document.getElementById('content').insertAdjacentHTML('beforeend', html);
+			document.getElementsByClassName('server--selected')[0].classList.remove('server--selected');
+			e.classList.add('server--selected');
+		}
+	}
+
+	var html = '';
+	for (var key in bg.per_country) {
+		var servers = bg.per_country[key];
+		for (var i = 0; i < servers.length; i++) {
+			if (i == 0) {
+				html += '<div class="country">' + bg.countries[key.replace(/^([a-zA-Z]+).*$/,"$1")] + '</div>';
+			}
+
+			html += '<div class="server ' + (bg.using == servers[i] ? 'server--selected' : '') + '" data-id="' + servers[i] + '">- ' + bg.replacements[servers[i]][1] + '</div>';
+		}
+	};
+	document.getElementById('content').insertAdjacentHTML('afterbegin', html);
 });
